a5978939182907fa1d26343b2e5bbbf6812cf3bd
LAPTOP-2T0TUTHF\Alex <mshevkun.dev@gmail.com>
2022-05-17 21:10:15 -0500

    Added media-queries

--- js/main.js
+++ js/main.js
@@ -1,14 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
   var burger = document.querySelector('.btn');
   var menu = document.querySelector('.full-menu');
-  var navItems = document.querySelector('.nav__item');
+  var navItems = document.querySelectorAll('.nav__item');
 
   burger.addEventListener('click', function () {
     burger.classList.toggle('btn--open');
     menu.classList.toggle('menu--open');
   });
 
-  navItems.onmouseover = function () {
-    navItems.not(document.querySelector(this)).classList.toggle('nav__item--hover');
+  navItems.forEach(el => {
+    el.onmouseover = function () {
+      burger.classList.toggle('btn--open');
+      menu.classList.toggle('menu--open');
+    };
+  });
+
+
+
+  // Change classes in contact-form section for max-width: 768px
+
+  const mql = window.matchMedia('(max-width: 768px)');
+  const contactForm = document.querySelector('.contact-form')
+  const contactFormWrapper = document.querySelector('.contact-form-content-wrapper')
+
+  function contactFormClassesChange(e) {
+    if (e.matches) {
+      if (contactForm.classList.contains('container-fluid')) {
+        contactForm.classList.remove('container-fluid')
+        contactForm.classList.add('container')
+      }
+
+      if (contactFormWrapper.classList.contains('row')) {
+        contactFormWrapper.classList.remove('row')
+        contactFormWrapper.classList.remove('d-flex')
+      }
+    } else {
+      if (contactForm.classList.contains('container')) {
+        contactForm.classList.remove('container')
+        contactForm.classList.add('container-fluid')
+      }
+
+      if (!contactFormWrapper.classList.contains('row')) {
+        contactFormWrapper.classList.add('row')
+        contactFormWrapper.classList.add('d-flex')
+      }
+    }
+  }
+
+  mql.onchange = (e) => {
+    contactFormClassesChange(e)
   };
+
+  contactFormClassesChange(mql)
+
+  // Close mobile munu after click on mobile nav-link
+
+  // const mobileLink = document.querySelector('.nav__link-mobile')
+  // const mobileNavMenu = document.querySelector('.full-menu')
+
+  // navItems.addEventListener('click', () => {
+  //   navItems.not(document.querySelector(this)).classList.toggle('nav__item--hover');
+  //   mobileNavMenu.classList.remove('menu--open')
+  // })
 });
